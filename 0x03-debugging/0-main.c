ad7b6c621c5fa0655f2514cd997df30a15757609
Okolo Michael <michaelchika58@outlook.com>
2023-06-22 08:09:21 -0700

    Debugging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
-/** main - Test function that prints if integer is possitive or negative
+/**
+ *  main - Test function that prints if integer is possitive or negative
  * Return: 0 successful
  */
 
 int main(void)
 {
 	int i;
+
 	i = 0;
 	postitive_or_negative(i);
 	return (0);
