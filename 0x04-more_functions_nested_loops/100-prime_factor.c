54b5b76aa16a1e55f3935b61887aa5b05b5ce1d1
Okolo Michael <michaelchika58@outlook.com>
2023-06-23 10:08:06 -0700

    prime number

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include <stdio>
+
+/**
+ * main - Prints the largest prime factor of a number.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int x, y;
+
+	x = 612852475143;
+	for (y = 2; y <= x; y++)
+	{
+		if (x % y == 0)
+		{
+			x /= y;
+			y--;
+		}
+	}
+	printf("%ld\n", y);
+	return (0);
+}
