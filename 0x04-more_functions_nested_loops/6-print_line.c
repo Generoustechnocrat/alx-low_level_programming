572ecbf2a7e3f63176d168e8a2d36c30e013e301
Okolo Michael <michaelchika58@outlook.com>
2023-06-23 07:52:54 -0700

    A function that prints straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - A function that draws straight lines
+ * @n: straight line is printed
+ * Return: Always return 0.
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n';)
+}
