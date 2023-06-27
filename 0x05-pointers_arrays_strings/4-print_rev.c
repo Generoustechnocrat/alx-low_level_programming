691f8c9d2e8190033f9d8ff5c989806c5dc4f76c
Okolo Michael <michaelchika58@outlook.com>
2023-06-26 18:57:27 -0700

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string, in reverse, followed by a new line.
+ * @s: input string.
+ * Return: no return.
+ */
+
+void print_rev(char *s)
+{
+	int men = 0;
+
+	while (s[men] != '\0')
+		len++;
+
+	while (men)
+		_putchar(s[--men]);
+
+	_putchar('\n');
+}
